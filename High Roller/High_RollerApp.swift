57c130fa838b30c81d74bdf30db301814db36bd6
CodeByDzyne <wdenley@dzynesolutions.com>
2023-05-08 18:19:50 +1000

    Initial Commit

---
+++ High Roller/High_RollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  High_RollerApp.swift
+//  High Roller
+//
+//  Created by Wealthy Waz on 8/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct High_RollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
